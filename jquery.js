4e1daeb13d76070dc3d0294bbe184b3e6117fe76
Steve Sartino <stevesartino@yahoo.com>
2015-09-11 14:55:06 -0500

    keyboard and spacebar work

--- jquery.js
+++ jquery.js
@@ -1,11 +1,30 @@
 $(document).ready(function(){
     
-    $( '#keyboard-upper-container' ).hide(function() {
-             $( ".div" ).keypress();
+    $( "#keyboard-upper-container" ).hide();
+    $( "#keyboard-lower-container" ).show();
+    
+    
+    $(document).keydown(function(event) {
+        if (event.which === 16) {
+            $( "#keyboard-lower-container" ).hide();
+            $( "#keyboard-upper-container" ).show();
+        }
     });
     
-    $( '#keyboard-lower-container' ).show(function() {
-             $( ".div" ).keypress();
+    $(document).keyup(function(event) {
+        if (event.which === 16) {
+            $( "#keyboard-upper-container" ).hide();
+            $( "#keyboard-lower-container" ).show();
+        }
     });
-      
+    
+    $(document).keypress(function(e)
+{
+    var s = String.fromCharCode(e.which);
+    if (s.match(/[a-zA-Z\.]/),([32]))
+        console.log(s + ' is a match!');
+});
+
+
+
 });
