94a620f3daa094e9c09fd933bf0540134ab92ece
Steve Sartino <stevesartino@yahoo.com>
2015-09-11 15:43:15 -0500

    Added Matt's changes for div fix.

--- jquery.js
+++ jquery.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
     
+    var sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
+        'Too ato too nOt enot one totA not anot tOO aNot',
+        'oat itain oat tain nate eate tea anne inant nean',
+        'itant eate anot eat nato inate eat anot tain eat',
+        'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+    var lines = [];
+    var lineCount = 0;
+    $(sentences).each(function(){
+        var letters = this.split('');
+        console.log(letters);
+        var line = $('<div class="letters"></div>');
+        $(letters).each(function() {
+        var letter = this;
+        if(letter == ' ') {
+            letter = '&nbsp;';
+        }
+       line.append($('<div class="letter"></div>').html(letter));
+        });
+        lines.push(line);
+    });
+
+    $('.test').append(lines[lineCount]);
+    
     $( "#keyboard-upper-container" ).hide();
     $( "#keyboard-lower-container" ).show();
     
@@ -18,12 +41,12 @@ $(document).ready(function(){
         }
     });
     
-    $(document).keypress(function(e)
-{
-    var s = String.fromCharCode(e.which);
-    if (s.match(/[a-zA-Z\.]/),([32]))
-        console.log(s + ' is a match!');
-});
+    $(document).keypress(function(e) { 
+        var s = String.fromCharCode(e.which);
+        if (s.match(/[a-zA-Z\.]/),([32]));
+            return
+        //console.log(s + ' is a match!');
+    });
 
 
 
